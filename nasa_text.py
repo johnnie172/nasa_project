9150610ee3ae9f07e918344108672aa8c5d50de3
Johnnie <jmpstillsart@gmail.com>
2020-05-11 18:42:11 +0300

    adding user inputs for date and hd

--- nasa_text.py
+++ nasa_text.py
@@ -1,27 +1,51 @@
 import requests
-import ast
+import datetime
+import json
+
+user_input_date = input('Would you like to get today pic or choose another date? (enter today/another): ').lower()
+if user_input_date[0] == 't':
+    date = datetime.datetime.now().date()
+else:
+    year = int(input('Enter a year(yyyy):'))
+    month = int(input('Enter a month(mm):'))
+    day = int(input('Enter a day(dd):'))
+    date = str(year)+"-"+str(month)+"-"+str(day)
+
 key = 'sRkSnDSlvzNR4VVZpHh31vM83gNB1ndbeAANCoId'
 
-response = requests.get('https://api.nasa.gov/planetary/apod?api_key=sRkSnDSlvzNR4VVZpHh31vM83gNB1ndbeAANCoId')
-response_text = response.text
-if response:
-    print('**Success!!')
 
+
+response = requests.get('https://api.nasa.gov/planetary/apod?api_key={}&date={}'.format(key, date))
+
+if response:
+    print('**Success!! you will soon get the pic!')
 else:
     print('An error has occurred.')
     print('Response cods: {}'.format(response))
 
 
-response_dir = ast.literal_eval(response_text)
-assert type(response_dir) is dict
+response_dict = json.loads(response.text)
+hd_url = response_dict["hdurl"]
+pic_url = response_dict["url"]
 
-hdurl = response_dir["hdurl"]
 
-
-user_input = input("Would you like to get the pic of today? ").lower()
-if user_input[0] == 'y':
-    Picture_request = requests.get('{}?api_key=sRkSnDSlvzNR4VVZpHh31vM83gNB1ndbeAANCoId'.format(hdurl))
+# user_input = input("Would you like to get the pic? ").lower()
+# if user_input[0] == 'y':
+user_input_hd = input("""Do you want it in HD or not? (enter "HD"/"not"): """).lower()
+if user_input_hd[0] == "h":
+    print('Please wait..')
+    Picture_request = requests.get('{}?api_key=sRkSnDSlvzNR4VVZpHh31vM83gNB1ndbeAANCoId'.format(hd_url))
+    if Picture_request.status_code == 200:
+        with open("C:/Users/jmpst/Downloads/image.jpg", 'wb') as f:
+            f.write(Picture_request.content)
+            print("You got the HD image!")
+else:
+    print('Please wait..')
+    Picture_request = requests.get('{}?api_key=sRkSnDSlvzNR4VVZpHh31vM83gNB1ndbeAANCoId'.format(pic_url))
     if Picture_request.status_code == 200:
         with open("C:/Users/jmpst/Downloads/image.jpg", 'wb') as f:
             f.write(Picture_request.content)
             print("You got the image!")
+
+
+
