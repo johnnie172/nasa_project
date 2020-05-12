49f7497ae75f01dcc0d590c742c2ddcff5594e1b
Johnnie <jmpstillsart@gmail.com>
2020-05-12 12:13:21 +0300

    changing dir to user path, fixing bugs

--- nasa_text.py
+++ nasa_text.py
@@ -2,16 +2,23 @@
 import requests
 import datetime
 import json
+import os
 
+
+user = os.getlogin()
+global date
 date = datetime.datetime.now().date()
 key = 'sRkSnDSlvzNR4VVZpHh31vM83gNB1ndbeAANCoId'
 
+
 def user_date():
-    year = int(input('Enter a year(yyyy): '))
-    month = int(input('Enter a month(mm): '))
-    day = int(input('Enter a day(dd): '))
-    date = str(year)+"-"+str(month)+"-"+str(day)
+    year = input('Enter a year(yyyy): ')
+    month = input('Enter a month(mm): ')
+    day = input('Enter a day(dd): ')
+    global date
+    date = (year)+"-"+(month)+"-"+(day)
     return date
+
 
 def choose_q(user_url):
 
@@ -19,7 +26,7 @@ def choose_q(user_url):
 
     Picture_request = requests.get('{}?api_key={}'.format(user_url, key))
     if Picture_request.status_code == 200:
-        with open("C:/Users/jmpst/Downloads/image.jpg", 'wb') as f:
+        with open("C:/Users/{}/Downloads/image.jpg".format(user), 'wb') as f:
             f.write(Picture_request.content)
 
             print("You got the image!")
@@ -32,14 +39,13 @@ if user_input_date[0] == 't':
 else:
     user_date()
 
-
 response = requests.get('https://api.nasa.gov/planetary/apod?api_key={}&date={}'.format(key, date))
 response_dict = json.loads(response.text)
 
 hd_url = response_dict["hdurl"]
 pic_url = response_dict["url"]
 
-
+#checking for code 200 (ok)
 if response:
     print('**Success!! you will soon get the pic!')
 else:
@@ -54,3 +60,5 @@ if user_input_hd[0] == "h":
       choose_q(hd_url)
 else:
     choose_q(pic_url)
+
+
